189a88642976dd01b9e648d761890ca84b4cd6c8
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-10-22 18:30:02 -0400

    Added intro

--- lottery/pages/index.tsx
+++ lottery/pages/index.tsx
@@ -1,27 +1,37 @@
 import type { NextPage } from "next";
-import dynamic from "next/dynamic";
 import { BlackTerminalPage } from "./components/BlackTerminalPage";
 import { Box, Flex } from "@chakra-ui/react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import { MultilineTypewriter } from "./components/MultilineTypewriter";
+import { Terminal } from "./components/Terminal";
 
 type State = "intro" | "interactive";
-
-const Terminal = dynamic(
-	() => import("../pages/components/TerminalComponent"),
-	{
-		ssr: false,
-	}
-);
-// create function that imports terminal
 
 const Home: NextPage = () => {
 	const [state, setState] = useState<State>("intro");
 
+	const intro = useMemo(
+		() => (
+			<MultilineTypewriter
+				texts={[
+					"W elcom e to Group 2's lottery",
+					"Follow the commands below to play",
+					"Hope the odds are in your favor",
+					". . .",
+				]}
+				onFinish={() => setState("interactive")}
+			/>
+		),
+		[]
+	);
+
 	return (
 		<BlackTerminalPage>
 			<Box h={5} />
-
-			{state === "interactive" && <Terminal />}
+			<Flex w="100%" flexDir="column">
+				{intro}
+			</Flex>
+			{state === "interactive" && <Terminal withHelp />}
 		</BlackTerminalPage>
 	);
 };
