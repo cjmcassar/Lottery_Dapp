d016728f917fe1fae7853be59a057516fb9bcc3c
Christopher-John Cassar <christopherjcassar@gmail.com>
2022-10-22 01:17:32 -0400

    *

--- lottery/pages/api/hello.ts
+++ lottery/pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+	name: string;
+};
 
 export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+	// listen for POST request
+	if (req.method === "POST") {
+		// process POST request and send response
+		res.status(200).json({ name: "John Doe" });
+	}
+	res.status(200).json({ name: "John Doe" });
 }
